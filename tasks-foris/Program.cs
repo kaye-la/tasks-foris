3455cbccf3ef8f1ae8afd9d53741deeaaac1eb8c
kaye-la <61543425+kaye-la@users.noreply.github.com>
2022-05-26 00:35:34 +0300

    Добавьте файлы проекта.

---
+++ tasks-foris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tasks_foris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tasks-foris/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace tasks_foris
+{
+	/// <summary>
+	/// 1.	Ломай меня полностью.
+	/// Реализуйте метод FailProcess так, чтобы процесс завершался.Предложите побольше различных решений.
+	/// </summary>
+	class Task1
+	{
+		static void Run()
+		{
+			try
+			{
+				FailProcess();
+			}
+			catch { }
+
+			Console.WriteLine("Failed to fail process!");
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Метод завершает текущий процесс программы
+		/// </summary>
+		static void FailProcess()
+		{
+			// Два метода, которые завершают текущий процесс:
+			// Environment.Exit(0);
+			Process.GetCurrentProcess().Kill();
+
+			// Либо как-нибудь сломать программу
+			FailProcess();
+		}
+	}
+}
